3a845376fda167ddada65610fc800aec94132f72
Hustlion <hustlionm@qq.com>
2016-02-23 21:34:13 +0800

    ssh

--- autoconfig.sh
+++ autoconfig.sh
@@ -7,7 +7,9 @@ for file in *.local; do ln -sf "`pwd`/$file" ~/".$file"; done
 ln -sf "`pwd`/git-flow-completion/git-flow-completion.zsh"  \
 ~/.git-flow-completion.zsh
 
-
+# Zsh Plugins
+## zsh-autosuggestions
+git clone git://github.com/tarruda/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
 # finish up
 rcup
